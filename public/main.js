846a2dd6b4f8b5d1cdd8fec133f8d32797caafc7
Kevin Unbedacht <KCUimac@Kevins-iMac-16306.local>
2017-04-11 11:44:09 -0600

    slider and images added

---
+++ public/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+  $('.slick-slider').slick({
+          autoplay: true,
+          arrows: true,
+          autoplaySpeed: 3500,
+          speed: 900,
+          dots: true
+      });
+
+
+
+
+
+
+
+  }); //.ready function
